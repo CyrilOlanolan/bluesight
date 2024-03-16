2422a196d575a2c2f745fca0a8adb3d3325d31da
Cyril Olanolan <57930177+CyrilOlanolan@users.noreply.github.com>
2024-03-16 11:13:37 +0800

    feat: initial project structure

---
+++ .eslintrc.js
@@ -0,0 +1,4 @@
+module.exports = {
+  extends: ['@cyrilolanolan/ts'],
+  root: true,
+};
---
+++ .prettierrc.js
@@ -0,0 +1,6 @@
+const sharedConfig = require('@cyrilolanolan/prettier-config');
+
+module.exports = {
+  ...sharedConfig,
+  // extend or overwrite options here
+};

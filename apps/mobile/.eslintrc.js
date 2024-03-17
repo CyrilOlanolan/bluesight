53a94d7eb0e93e684f3fdb49e16016e7879153df
Cyril Olanolan <57930177+CyrilOlanolan@users.noreply.github.com>
2024-03-17 21:20:47 +0800

    fix(ci): fixed eslint in mobile and api projects

--- apps/mobile/.eslintrc.js
+++ apps/mobile/.eslintrc.js
@@ -1,6 +1,11 @@
 module.exports = {
   extends: ['../../.eslintrc.js'],
   settings: {
-    'import/ignore': ['node_modules/react-native/index\\.js$'],
+    'import/ignore': ['node_modules/react-native/index\\.js$'], // Recommended fix based on the forums
+    'import/resolver': {
+      typescript: {
+        project: ['./src'],
+      },
+    },
   },
 };

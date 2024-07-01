7111db87599d009d9cc264b53e2daeaf199b1f2b
Innhahn <jaytan3825@gmail.com>
2024-07-01 18:21:18 +0800

    refactor(#2): fixed Eslint Issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,7 @@
 module.exports = {
-  extends: ['@cyrilolanolan/ts', 'plugin:import/typescript'],
+  extends: ['@cyrilolanolan/ts'],
   settings: {
-    'import/ignore': ['node_modules', 'react-native'],
     'import/resolver': {
-      node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      },
-
       typescript: {
         project: [
           'apps/**/tsconfig.json',
@@ -16,14 +11,4 @@ module.exports = {
       },
     },
   },
-  ignorePatterns: ['node_modules/**'],
-  // Add the following ignore pattern to include specific files or directories
-  overrides: [
-    {
-      files: ['.storybook/**/*.ts', '.storybook/**/*.tsx'],
-      rules: {
-        // specific rules for .storybook files
-      },
-    },
-  ],
 };
